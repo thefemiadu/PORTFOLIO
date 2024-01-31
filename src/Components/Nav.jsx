6b87d54938b453ccf4b43df2d17517bd26f3efd3
thefemiadu <51876723+thefemiadu@users.noreply.github.com>
2024-01-30 21:19:43 -0500

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -38,7 +38,7 @@ const Nav = () => {
         <nav>
             <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 border-b border-slate-800">
                 <div className="flex items-center flex-1">
-                    <span className="text-3xl font-bold">Logo</span>
+                    <span className="text-3xl font-bold">XYLINE</span>
                 </div>
                 <div className="lg:flex md:flex lg:flex-1 items center justify-end font-normal hidden ">
                     <div className="flex-10">
